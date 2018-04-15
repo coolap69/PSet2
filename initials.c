4f2d34bf4c22f72627d8ede81c76b60bd47f8939
Louis Pazos <coolap69@aol.com>
2018-04-15 22:52:40 +0000

    initials works

--- initials.c
+++ initials.c
@@ -1,19 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int main (void)
+int main(void)
 {
-    string s = get_string("Name: ");
-    for (int i = 0; i < strlen(s); i++)
-    {
-        printf("%c %i\n", s[i], (int) s[i]);
-    }
-}
+    string name = get_string("Name: ");
 
-// prompt for name
-//     get_string
-//     assume only letters and spaces
-//     assume single space between
+    //print first initial
+    printf("initials: %c", toupper(name[0]));
+
+    for (int i = 0; i < strlen(name); i++)
+    {
+        // creating space, +1 jumpt from empty space, false empty space !='\0'
+        if (name [i] == ' ' && name[i + 1] != '\0')
+        {
+            printf("%c", toupper(name[i + 1]));
+        }
+    }
 
 // print initials
+    printf("\n");
+}
+
