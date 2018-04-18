1c78ff7da85d29b07a32ed6ea31faaae98c0338e
Louis Pazos <coolap69@aol.com>
2018-04-18 00:56:36 +0000

    passing check50

--- 02Caesar/caesar.c
+++ 02Caesar/caesar.c
@@ -7,8 +7,8 @@
 int main(int argc, string argv[])
 
 {
-     // key from command line argument
-     int key;
+    // key from command line argument
+    int key;
 
     //accepts 2 arguments
     // name of the program
@@ -17,40 +17,51 @@ int main(int argc, string argv[])
     {
         printf("Key: %s\n", argv[1]);
     }
+    else
+        //returning an error if i don't use a letter after ./caesar
+    {
+        printf("Usage: ./caesar k \n");
+        return 1;
+    }
+
     //turn key into integer
     key = atoi(argv[1]);
 
     // prompt for plaintext
     string plaintext = get_string("Plaintext: ");
-    // {
-    //     printf("Your last string was %s\n", plaintext);
-    // }
-        for (int i = 0; i < strlen(plaintext); i++)
+    {
+        printf("ciphertext: ");
+    }
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (!isalpha(plaintext[i]))
         {
-            if (!isalpha(plaintext[i]))
-            {
             printf("%c", plaintext[i]);
-            }
-            else if (isupper(plaintext[i]))
-            {
-            printf("%c", (((plaintext[i] - 'A') +key) %26 + 'A'));
-            }
-            else if (islower(plaintext[i]))
-            {
-            printf ("%c", (((plaintext[i] - 'A') +key) %26 + 'A'));
-            }
-
         }
-        printf ("\n");
+        else if (isupper(plaintext[i]))
+        {
+            printf("%c", (((plaintext[i] - 'A') + key) % 26 + 'A'));
+        }
+        else if (islower(plaintext[i]))
+        {
+            printf("%c", (((plaintext[i] - 'a') + key) % 26 + 'a'));
+        }
 
     }
+    printf("\n");
+
+    //returning a 0 if there is no errors
+    return 0;
+
+}
 
 // TODO
 // get the key
-    // 2nd command line argument
-    // function atoi : from string to integer
-    // string num = "50";
-    // int i = atoi(num);
+// 2nd command line argument
+// function atoi : from string to integer
+// string num = "50";
+// int i = atoi(num);
+
 // get the plaintext
 // encipher
 // print ciphertext
@@ -61,7 +72,7 @@ int main(int argc, string argv[])
 // turn key into integer
 // prompt for plaintext
 // for each character in the plaintext string
-  // if aphabetic
-  // preserve case
+// if aphabetic
+// preserve case
 // shift plaintext character by key
 // print ciphertext
